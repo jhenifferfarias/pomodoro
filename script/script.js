b044752913d93dd65cf407f3d655deaa8de42738
Jheniffer Farias <jhenifferfarias.dev@gmail.com>
2023-01-24 20:51:54 -0300

    terminando js do contador

--- script/script.js
+++ script/script.js
@@ -1,14 +1,39 @@
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const resetButton = document.getElementById('reset');
+let TIMER = null;                
 
+
+playButton.addEventListener('click', startCounter);
+pauseButton.addEventListener('click', pauseCounter);
+resetButton.addEventListener('click', resetCounter);
 
 const INITIAL_TIME_IN_SECONDS = 25 * 60;
 let TIME_IN_SECONDS = INITIAL_TIME_IN_SECONDS;
 
-setInterval( () => {
-    TIME_IN_SECONDS--;
-    
+updateDom();
+
+function startCounter() {
+    TIMER = setInterval( () => {
+        TIME_IN_SECONDS--;
+        updateDom();
+    }, 1000); 
+} 
+
+function pauseCounter() {
+    clearInterval(TIMER); 
+}
+
+function resetCounter() {
+    clearInterval(TIMER); 
+    TIME_IN_SECONDS = INITIAL_TIME_IN_SECONDS;
+    updateDom();
+}
+
+function updateDom() {
     const minutes = Math.floor(TIME_IN_SECONDS / 60);
     document.getElementById('min').innerHTML = String(minutes).padStart(2, 0);
-
+    
     const seconds = TIME_IN_SECONDS % 60;
     document.getElementById('sec').innerHTML = String(seconds).padStart(2, 0);
-}, 1000); 
+}
