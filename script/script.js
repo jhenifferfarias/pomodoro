e13ae4412ad5d83b151030ac32cbe3cd1aba95a6
Jheniffer Farias <jhenifferfarias.dev@gmail.com>
2023-01-24 20:25:14 -0300

    incluindo js no relógio

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
-console.log('oi');
+
+
+const INITIAL_TIME_IN_SECONDS = 25 * 60;
+let TIME_IN_SECONDS = INITIAL_TIME_IN_SECONDS;
+
+setInterval( () => {
+    TIME_IN_SECONDS--;
+    
+    const minutes = Math.floor(TIME_IN_SECONDS / 60);
+    document.getElementById('min').innerHTML = String(minutes).padStart(2, 0);
+
+    const seconds = TIME_IN_SECONDS % 60;
+    document.getElementById('sec').innerHTML = String(seconds).padStart(2, 0);
+}, 1000); 
